ae4c86f27d31171b03753663332017283c5f8d6f
hovovardanyan <57057585+hovovardanyan@users.noreply.github.com>
2020-02-29 12:36:43 +0400

    Добавьте файлы проекта.

---
+++ Forum_Final/Interfaces/Post-Comment/IMassage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum_Final.Interfaces.Post_Comment
+{
+    interface IMassage
+    {
+        // Upload
+        // Delete
+        // Edit
+    }
+}
