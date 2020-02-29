e1e21a387f067c83cc0f9f5ddce9b5eed294c72f
deadartt <deadartt@gmail.com>
2020-02-29 13:02:01 +0400

    Users Db and class creating

---
+++ Forum_Final/Users/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum_Final
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<Users> dataUsers { get; set; } // creating Db for Users, and using this we can add,delete and save users in our Db
+    }
+}
