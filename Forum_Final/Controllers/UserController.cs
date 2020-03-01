53c73be8fa9593c6e9163d8c84db88e40dd8932c
Suren <surenbarseghyan13@gmail.com>
2020-03-01 14:40:45 +0400

    Register Login

---
+++ Forum_Final/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forum_Final.Models;
+namespace Forum_Final.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "UserName,UserSurname,UserLogin,UserPassword")]User user) 
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index","Home");
+
+            }
+            return View();
+        
+        }
+
+        public ActionResult Login() 
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Register");
+            }
+            return View();
+        }
+    }
+}
--- Forum_Final/Models/User.cs
+++ Forum_Final/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,24 @@ namespace Forum_Final.Models
     public class User
     {
         public int UserId = 0; // unique user Id
+
+        //Display is need for showing property names for UI
+        [Display(Name ="First Name")]
         public string UserName { get; private set; }
+        [Display(Name = "Last Name")]
+
         public string UserSurname { get; private set; }
-        public string UserLogin { get; private set; }
+        [Display(Name = "Username")]
+       public string UserLogin { get; private set; }
+        [Display(Name = "Password")]
+
         public string UserPassword { get; private set; }
+        
+        
+        public User()
+        {
 
-
+        }
         //creating user
         public User(string name, string surname, string login, string password)
         {
