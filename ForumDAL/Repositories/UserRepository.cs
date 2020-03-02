2a20fd941b1fab21203c5f6ba3b7e40c5fadfa48
Suren <surenbarseghyan13@gmail.com>
2020-03-02 18:34:26 +0400

    UserRepository

--- ForumDAL/Repositories/UserRepository.cs
+++ ForumDAL/Repositories/UserRepository.cs
@@ -8,5 +8,33 @@ namespace ForumDAL.Repositories
 {
     class UserRepository
     {
+        ForumContext context;
+        public UserRepository()
+        {
+            context = new ForumContext();
+        }
+
+        public void AddUser(User user)
+        {
+            context.usersData.Add(user);
+
+            context.SaveChanges();
+
+        }
+        public User GetById(int id)
+        {
+            var user = context.usersData.Where(u => u.UserId == id).FirstOrDefault();
+            return user;
+
+
+        }
+        public User SignIn(string username, string password)
+        {
+            var user = context.usersData.Where(u => u.UserLogin == username && u.UserPassword == password).FirstOrDefault();
+            return user;
+
+        }
+        
+
     }
 }
