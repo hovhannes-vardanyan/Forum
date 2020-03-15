88a74cac4f727792d0cb8eb928dedc3d56955edd
hovovardanyan <57057585+hovovardanyan@users.noreply.github.com>
2020-03-15 19:29:01 +0400

    searchUI

---
+++ Forum_Final/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using ForumDAL;
+using ForumDAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum_Final.Controllers
+{
+    public class SearchController : Controller
+    {
+        UnitOfWork unitOfWork = new UnitOfWork(new ForumContext());
+
+        public ActionResult Search()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Search(string searchString)
+        {
+
+            ViewBag.SearchString = searchString;
+            return RedirectToAction("SearchResultPage");
+
+        }
+
+
+        [HttpGet]
+        public ActionResult SearchResultPage()
+        {
+            ViewBag.Result = unitOfWork.SearchRepository.Search(ViewBag.SearchString);
+            return View();
+        }
+    }
+}
